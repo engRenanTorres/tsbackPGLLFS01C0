fce42abb0527a13fd952dfdb2a00cb7d990bc1c6
Renan Torres <engrtorres@outlook.com>
2024-08-21 19:28:10 -0300

    feat: Configuração de testes com jest + repositorio e bd fake

--- src/infra/usuarioRepositorio.ts
+++ src/infra/usuarioRepositorio.ts
@@ -1,11 +1,11 @@
 import path from 'path';
 import fs from 'fs';
 import DBSchema from './dbSchema';
-import { UsarioSchema } from './usuarioSchema';
+import { UsarioSchema as UsuarioSchema } from './usuarioSchema';
+import { AtualizarUsuarioDTO, CriarUsuarioDTO } from '../usuario-dto';
+import { UsuarioModel } from '../entidades/usuarios';
 
-// SQLite
-
-class UsarioRepositorio {
+class UsuarioRepositorio {
   private readonly caminhoArquivo: string;
 
   constructor () {
@@ -17,31 +17,67 @@ class UsarioRepositorio {
     return JSON.parse(bdPuro);
   }
 
-  private reescreverArquivo (DBAtualizado: DBSchema):boolean {
+  private reescreverUsuariosNoArquivo (usuarios: Array<UsuarioSchema>):boolean {
+    const bd = this.acessoBD();
+    bd.users = usuarios;
     try {
-      fs.writeFileSync(this.caminhoArquivo, JSON.stringify(DBAtualizado));
+      fs.writeFileSync(this.caminhoArquivo, JSON.stringify(bd));
       return true;
     } catch {
       return false;
     }
   }
 
-  public buscaTodos (): UsarioSchema[] {
+  public buscaTodos (): UsuarioSchema[] {
     const bd = this.acessoBD();
     return bd.users;
   }
 
-  public buscaPorId (id: number): UsarioSchema | undefined {
+  public buscaPorId (id: number): UsuarioSchema | undefined {
     const bd = this.acessoBD();
     const usuario = bd.users.find((usuario) => usuario.id === id);
     return usuario;
   }
 
-  public criar (usario: UsarioSchema) {
-    const bd = this.acessoBD();
-    bd.users.push(usario);
-    this.reescreverArquivo(bd);
+  public criar (usario: CriarUsuarioDTO) {
+    const usuarios = this.buscaTodos();
+
+    const usarioMaiorId = usuarios.reduce(
+      (max, usario) => usario.id > max.id ? usario : max, usuarios[0]
+    );
+
+    const novoUsuario = new UsuarioModel(
+      usarioMaiorId.id + 1,
+      usario.nome,
+      usario.ativo
+    );
+    usuarios.push(novoUsuario);
+    this.reescreverUsuariosNoArquivo(usuarios);
+  }
+
+  public atualizar (id:number, dadosNovos: AtualizarUsuarioDTO) {
+    const usuarios = this.buscaTodos();
+    const posicaoUsuario = usuarios.findIndex(usuario => usuario.id === id);
+    if (posicaoUsuario !== -1) {
+      if (dadosNovos.nome) {
+        usuarios[posicaoUsuario].nome = dadosNovos.nome;
+      }
+      if (dadosNovos.ativo !== undefined) {
+        usuarios[posicaoUsuario].ativo = dadosNovos.ativo;
+      }
+      this.reescreverUsuariosNoArquivo(usuarios);
+    }
+  }
+
+  public deletar (id: number) {
+    const usuarios = this.buscaTodos();
+    const posicaoUsuario = usuarios.findIndex(usuario => usuario.id === id);
+    if (posicaoUsuario !== -1) {
+      usuarios.splice(posicaoUsuario, 1);
+      this.reescreverUsuariosNoArquivo(usuarios);
+    }
   }
 }
 
-export default UsarioRepositorio;
+export default UsuarioRepositorio;
+
